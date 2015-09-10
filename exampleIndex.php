b38346faf959833333bfda8993b2a2e70f451a0f
DrRoach <ryandeas1@gmail.com>
2015-09-10 19:27:11 +0100

    Updated the example file

--- exampleIndex.php
+++ exampleIndex.php
@@ -10,16 +10,39 @@
     ball.setSprite('ball.png', 50, 50);
     ball.draw();
 
-    Screen.setFPS(60);
+    var player1 = new Object();
+    player1.setSprite('paddle.png', 20, 60);
+    player1.setPosition(Screen.LEFT, 50);
+
+    var player2 = new Object();
+    player2.setSprite('paddle.png', 20, 60);
+    player2.setPosition(Screen.RIGHT, 50);
+
+    var moving = false;
+
+    player2.keyHold(83, function() {
+        player2.move('south', 5);
+    });
+    player2.keyHold(87, function() {
+        player2.move('north', 5);
+    });
 
     var i = 0;
     setInterval(function() {
         Screen.clear();
 
-        if (i++ < 500) {
-            ball.move('east', 2);
+        if (moving) {
+            player2.move('south', 5);
         }
+
+        if (i++ < 100) {
+            ball.move('southeast', 2);
+            console.log(player2.keys);
+        }
+
         ball.draw();
+        player1.draw();
+        player2.draw();
     }, Screen.TICK);
 </script>
 </body>
